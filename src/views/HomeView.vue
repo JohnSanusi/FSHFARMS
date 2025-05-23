41a01e29a8af4c8ec0d562f09d5eff4811f0d171
JohnSanusi <johnsanusi18@gmail.com>
2025-05-23 12:36:59 +0100

    Started FHSFARMS Project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import Sidebar from " @/components/Sidebar.vue";
+import About from "@/components/About.vue";
+import HomeGallery from "@/components/Home-Gallery.vue";
+import Products from "@/components/Products.vue";
+import Contact from "@/components/Contact.vue";
+import Footer from "@components/Footer.vue";
+</script>
+
+<template>
+  <header>
+    <Sidebar />
+    <Hero />
+  </header>
+  <main>
+    <About />
+    <Products />
+    <HomeGallery />
+    <Contact />
+  </main>
+  <footer>
+    <Footer />
+  </footer>
+</template>
